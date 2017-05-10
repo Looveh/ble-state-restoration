a6f947a1296334cf4a32b0223c9846a152c158f1
Niclas Nilsson <niclas.love.nilsson@gmail.com>
2017-05-10 13:00:34 +0200

    Skeleton project for connecting via ble

--- index.ios.js
+++ index.ios.js
@@ -8,23 +8,91 @@ import React, { Component } from 'react';
 import {
   AppRegistry,
   StyleSheet,
+  NativeAppEventEmitter,
   Text,
   View
 } from 'react-native';
+import BleManager from 'react-native-ble-manager';
+import PushNotification from "react-native-push-notification";
+
+const serviceUUID = "1814";
+const charUUID = "2A53";
+
+PushNotification.localNotification({
+  message: "parsing file"
+});
 
 export default class BleStateRestoration extends Component {
+
+  componentDidMount() {
+    PushNotification.configure({
+      requestPermission: true
+    });
+
+    PushNotification.localNotification({
+      message: "componentDidMount"
+    });
+
+    BleManager.start({
+      restoreIdentifierKey: "BleStateRestorationKey"
+    });
+
+    NativeAppEventEmitter.addListener(
+      "BleManagerDiscoverPeripheral",
+      this.handleDiscoverPeripheral
+    );
+
+    NativeAppEventEmitter.addListener(
+      "BleManagerDidUpdateState",
+      this.handleDidUpdateState
+    );
+
+    NativeAppEventEmitter.addListener(
+      "BleManagerDidUpdateValueForCharacteristic",
+      this.handleDidUpdateValueForCharacteristic
+    );
+
+    BleManager.checkState();
+  }
+
+  handleDiscoverPeripheral(p) {
+    console.log('handleDiscoverPeripheral: ', p);
+
+    BleManager.connect(p.id)
+      .then((pInfo) => {
+        console.log("connected: ", pInfo);
+
+        return BleManager.startNotification(p.id, serviceUUID, charUUID);
+      })
+      .then(() => {
+        console.log("notification started");
+      })
+      .catch((err) => {
+        console.err("connect error:", err);
+      });
+  }
+
+  handleDidUpdateState({state}) {
+    console.log("handleDidUpdateState: ", state);
+
+    if (state === "on") {
+      BleManager.scan([serviceUUID], 30, false);
+    }
+  }
+
+  handleDidUpdateValueForCharacteristic(value) {
+    console.log("handleDidUpdateValueForCharacteristic: ", value);
+
+    PushNotification.localNotification({
+      message: `Value: ${value.value}`
+    });
+  }
+
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
           Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
         </Text>
       </View>
     );
@@ -42,11 +110,6 @@ const styles = StyleSheet.create({
     fontSize: 20,
     textAlign: 'center',
     margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
   },
 });
 
